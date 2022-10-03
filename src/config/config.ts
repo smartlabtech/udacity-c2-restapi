e2bc5fe93c11f4f8585c26490b97967461cf6c95
smartlabtech <michaelyoussif.elias@gmail.com>
2022-10-03 12:20:50 +0200

    New Project

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "database1",
+    "password": "database1",
+    "database": "postgres",
+    "host": "database1.c9gerjxmrrbk.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "555295456891-projectx-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
