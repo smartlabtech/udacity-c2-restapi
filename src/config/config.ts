abe93c5d8a88a5dd2690fefb23f1fd83bf7add88
smartlabtech <michaelyoussif.elias@gmail.com>
2022-10-03 19:10:48 +0200

    Enhancements

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,19 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
   "dev": {
-    "username": "database1",
-    "password": "database1",
-    "database": "postgres",
-    "host": "database1.c9gerjxmrrbk.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "555295456891-projectx-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
